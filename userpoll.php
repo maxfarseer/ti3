d71fd964a9691ddf3ec2560bd36fb412bdb4ed89
Maxim.Patsiansky <maxim.patsiansky@gmail.com>
2013-06-24 16:57:49 +0400

    full backup FTP 24.06.2013

--- userpoll.php
+++ userpoll.php
@@ -1,6 +1,14 @@
 <?php 
+/*echo json_encode(array(
+    'ok' => true,
+    'x' => 41,
+    'y' => 2
+));
+echo $_POST['myData'];*/
 
-// шамань тут сам =)
-//  у меня на success стоит data, по идее должно че-то вернуть, если ты тут это укажешь
+$obj = json_decode($_POST['myData'],true);
+foreach ($obj as $key=>$val) {
+echo '<br>'.$key.'  '.$val;
+}
 
 ?>
